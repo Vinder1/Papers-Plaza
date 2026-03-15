64dcca2759756ae00fcaf6e1115c301940ed2af6
VinderX <kar.galiullin@gmail.com>
2026-03-15 18:01:31 +0300

    dump and seeds

--- s2/wal.sql
+++ s2/wal.sql
@@ -22,6 +22,51 @@ INSERT INTO test_wal (val) SELECT md5(random()::text) FROM generate_series(1, 10
 
 
 -- 3. DUMP
+-- создание:
+docker compose run postgres pg_dump -h postgres -U admin PapersPlease > dump.sql
+-- dump весит больше пол гигабайта, поэтому в репозитории его не будет
+-- скриншот в wal_lsn_screenshots/dump.png
+
+-- Дальше была создана вторая бд на порту 5433
+-- туда был перенесён дамп
+-- загрузка дампа:
+docker compose exec -T postgres psql -U postgres -f /backup/dump.sql
+
+-- результат в dump_restored.png и dump_restored2.png
+-- копировалась база данных полностью, вместе с данными
+
+docker compose down -v
+-- удалить всё обратно
+
+-- аналогично, только для исключительно структуры:
+docker compose run --rm postgres pg_dump -h postgres -U admin --schema-only PapersPlease > schema_only.sql
+
+-- для одной таблицы:
+docker compose run --rm postgres pg_dump -h postgres -U admin --table users PapersPlease > table_users.sql
 
 
 
+-- 4. SEED
+
+-- добавление происходит с проверкой, что под такими id никого нет
+INSERT INTO identity.country (id, name)
+VALUES (1, 'Россия'),
+    (2, 'США'),
+    (3, 'КНДР'),
+    (4, 'Китай'),
+    (5, 'Казахстан'),
+    (6, 'Германия'),
+    (7, 'Афганистан'),
+    (8, 'Мексика'),
+    (9, 'Нигер')
+ON CONFLICT (id) DO NOTHING;
+
+-- или так, по имени, но по одному
+INSERT INTO identity.country (name)
+SELECT 'Россия'
+WHERE NOT EXISTS
+(
+    SELECT 1 FROM identity.country WHERE name = 'Россия'
+);
+
+-- и такое надо сделать со всеми запросами в GenerateBigData, но я не буду
