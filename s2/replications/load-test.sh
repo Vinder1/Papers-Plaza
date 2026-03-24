a9b3087a14c12a287f6b0919ae5d99595da8dfb6
VinderX <kar.galiullin@gmail.com>
2026-03-24 19:15:07 +0300

    replication done

---
+++ s2/replications/load-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Начало нагрузки INSERT на Master..."
+
+for i in {1..1000}; do
+    docker exec pg-master psql -U postgres -d testdb -c \
+        "INSERT INTO test_data (name) VALUES ('Load Test $i');"
+    
+    # Проверка лага каждые 10 записей
+    if [ $((i % 10)) -eq 0 ]; then
+        echo "=== Lag после $i записей ==="
+        docker exec pg-master psql -U postgres -d testdb -c "
+        SELECT 
+            client_addr,
+            pg_wal_lsn_diff(sent_lsn, replay_lsn) AS lag_bytes
+        FROM pg_stat_replication;
+        "
+
+        docker exec pg-replica1 psql -U postgres -d testdb -c \
+        "SELECT COUNT(*) FROM test_data;"
+    fi
+done
+
+echo "Нагрузка завершена!"
